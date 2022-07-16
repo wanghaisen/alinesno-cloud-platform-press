f1dad9b238f18bd40d0605bde5a7af43e738a45f
weixiaojin0712 <wendyweyyy@qq.com>
2022-07-16 23:33:36 +0000

    fix:构建无参数的问题

--- .algolia/scraper.sh
+++ .algolia/scraper.sh
@@ -5,4 +5,8 @@ fi
 pip install --upgrade 'algoliasearch>=2.0,<3.0'
 python clear_index.py
 
-docker run -i --env-file=.env -e "CONFIG=$(cat config.json | jq -r tostring)" algolia/docsearch-scraper
+docker run \
+    -e APPLICATION_ID=$(printenv ALGOLIA_APPLICATION_ID) \
+    -e API_KEY=$(printenv ALGOLIA_WRITE_API_KEY) \
+    -e "CONFIG=$(cat config.json | jq -r tostring)" \
+    algolia/docsearch-scraper:v1.13.0
