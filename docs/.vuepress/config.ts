e2071790a90abedaacd03153213f84cb824c47d4
LuoAnDong <luoandong@gmail.com>
2022-07-16 19:06:15 +0800

    feat:添加algolia

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,10 +3,11 @@ import { localTheme } from './theme'
 const { path } = require('@vuepress/utils')
 
 const { nprogressPlugin } = require('@vuepress/plugin-nprogress')
-import { docsearchPlugin } from '@vuepress/plugin-docsearch'
+const { docsearchPlugin } = require('@vuepress/plugin-docsearch')
 const { googleAnalyticsPlugin } = require('@vuepress/plugin-google-analytics')
-import { clipboardPlugin } from "vuepress-plugin-clipboard";
 const { mediumZoomPlugin } = require('@vuepress/plugin-medium-zoom')
+
+import { clipboardPlugin } from "vuepress-plugin-clipboard";
 import { mdEnhancePlugin } from "vuepress-plugin-md-enhance";
 import { feedPlugin } from "vuepress-plugin-feed2";
 import { sitemapPlugin } from "vuepress-plugin-sitemap2";
@@ -23,15 +24,15 @@ export default defineUserConfig({
   ],
   plugins: [
     clipboardPlugin({
-      align: 'top' , 
-      color: '#fff' , 
-      successText: '复制成功!' , 
-      successTextColor: '#fff' 
+      align: 'top' ,
+      color: '#fff' ,
+      successText: '复制成功!' ,
+      successTextColor: '#fff'
     }),
     sitemapPlugin({
       // 配置选项
-      hostname: 'http://alinesno-platform.linesno.com' , 
-      changefreq: 'daily' , 
+      hostname: 'http://alinesno-platform.linesno.com' ,
+      changefreq: 'daily' ,
 
     }),
     // mdEnhancePlugin({
@@ -45,10 +46,10 @@ export default defineUserConfig({
     }),
     feedPlugin({
       // 插件选项
-      hostname: 'http://alinesno-platform.linesno.com' , 
-      json: true , 
-      atom: true , 
-      rss: true , 
+      hostname: 'http://alinesno-platform.linesno.com' ,
+      json: true ,
+      atom: true ,
+      rss: true ,
     }),
     mediumZoomPlugin({
       // 配置项
@@ -56,51 +57,48 @@ export default defineUserConfig({
     '@renovamen/vuepress-plugin-baidu-tongji', {
       'ba': 'd56b5be4e3fa14164ffdc68ade2beef9'
     },
-    docsearchPlugin({ 
+    docsearchPlugin({
       appId: 'SI4XZK527J',
-      apiKey: 'de3e76371cc64406463be7bffa324acf',
-      indexName: 'alinesno-cloud-platform',
-      searchParameters: {
-        attributesToSnippet: ["lvl0:30", "content:25"],
-      },
+      apiKey: 'b0df382dd6e02495ccca231de710a0c3',
+      indexName: 'acp',
       locales: {
-        "/": {
-          placeholder: "search docs",
+        '/zh/': {
+          placeholder: '搜索文档',
           translations: {
             button: {
-              buttonText: "搜索文档",
-              buttonAriaLabel: "搜索文档",
+              buttonText: '搜索文档',
+              buttonAriaLabel: '搜索文档',
             },
             modal: {
               searchBox: {
-                resetButtonTitle: "清除查询条件",
-                resetButtonAriaLabel: "清除查询条件",
-                cancelButtonText: "取消",
-                cancelButtonAriaLabel: "取消",
+                resetButtonTitle: '清除查询条件',
+                resetButtonAriaLabel: '清除查询条件',
+                cancelButtonText: '取消',
+                cancelButtonAriaLabel: '取消',
               },
               startScreen: {
-                recentSearchesTitle: "搜索历史",
-                noRecentSearchesText: "没有搜索历史",
-                saveRecentSearchButtonTitle: "保存至搜索历史",
-                removeRecentSearchButtonTitle: "从搜索历史中移除",
-                favoriteSearchesTitle: "收藏",
-                removeFavoriteSearchButtonTitle: "从收藏中移除",
+                recentSearchesTitle: '搜索历史',
+                noRecentSearchesText: '没有搜索历史',
+                saveRecentSearchButtonTitle: '保存至搜索历史',
+                removeRecentSearchButtonTitle: '从搜索历史中移除',
+                favoriteSearchesTitle: '收藏',
+                removeFavoriteSearchButtonTitle: '从收藏中移除',
               },
               errorScreen: {
-                titleText: "无法获取结果",
-                helpText: "你可能需要检查你的网络连接",
+                titleText: '无法获取结果',
+                helpText: '你可能需要检查你的网络连接',
               },
               footer: {
-                selectText: "选择",
-                navigateText: "切换",
-                closeText: "关闭",
-                searchByText: "搜索提供者",
+                selectText: '选择',
+                navigateText: '切换',
+                closeText: '关闭',
+                searchByText: '搜索提供者',
               },
               noResultsScreen: {
-                noResultsText: "无法找到相关结果",
-                suggestedQueryText: "你可以尝试查询",
-                reportMissingResultsText: "你认为该查询应该有结果？",
-                reportMissingResultsLinkText: "点击反馈",
+                noResultsText: '无法找到相关结果',
+                suggestedQueryText: '你可以尝试查询',
+                reportMissingResultsText: '你认为该查询应该有结果？',
+                reportMissingResultsLinkText: '点击反馈',
               },
             },
           },
@@ -121,7 +119,7 @@ export default defineUserConfig({
     //   },
     // }),
   ],
-  
+
   theme: localTheme({
     logo: '/logo_1.png', // 注意图片放在 public 文件夹下
     docsDir: 'docs',
@@ -131,9 +129,9 @@ export default defineUserConfig({
     editLink: true,
     // editLinkText: '编辑页面',
     sidebarDepth: 0,
-    lastUpdated: true, 
+    lastUpdated: true,
     lastUpdatedText: 'Last Updated',
-    contributors: false, 
+    contributors: false,
     navbar: [
       // 嵌套 Group - 最大深度为 2
       {
@@ -1958,7 +1956,7 @@ function genSolutionSidebar(menus) {
   if (menus == 0) {
     // 平台介绍
     const mapArr = [
-      '/solution/' , 
+      '/solution/' ,
       '/solution/26_企业数字中台整体方案.md',
     ]
 
