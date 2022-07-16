9a4fe536bc23e65df3aa213210140549e3e43458
weixiaojin0712 <wendyweyyy@qq.com>
2022-07-16 23:20:57 +0000

    feat:调整项目配置

--- .algolia/scraper.sh
+++ .algolia/scraper.sh
@@ -5,8 +5,4 @@ fi
 pip install --upgrade 'algoliasearch>=2.0,<3.0'
 python clear_index.py
 
-docker run \
-    -e APPLICATION_ID=$(printenv ALGOLIA_APPLICATION_ID) \
-    -e API_KEY=$(printenv ALGOLIA_WRITE_API_KEY) \
-    -e CONFIG="$(cat config.json | jq '.start_urls=[env.ALGOLIA_SITE_URL]' | jq '.index_name=env.ALGOLIA_INDEX_NAME' | jq -r tostring)" \
-    algolia/docsearch-scraper:v1.13.0
+docker run -it --env-file=.env -e "CONFIG=$(cat config.json | jq -r tostring)" algolia/docsearch-scraper
