e8477c2cc128d85cfc8728d1b64f303897d29abc
weixiaojin0712 <wendyweyyy@qq.com>
2022-07-17 16:51:13 +0000

    feat:添加商务企业微信

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2028,7 +2028,7 @@ function genPricesSidebar(menus) {
     })
   } else if (menus == 1) {
     // 平台介绍
-    const mapArr = ['/prices/01_版本内容.md', '/prices/02_授权协议.md']
+    const mapArr = ['/prices/01_版本内容.md', '/prices/02_授权协议.md' , '/prices/04_商务沟通.md']
 
     return mapArr.map(i => {
       return i
