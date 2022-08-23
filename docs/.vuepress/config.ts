a24b0e836eb137b739b8d3828462bd0fb446dbba
siyuedefeng <suze7777777@163.com>
2022-08-23 14:33:01 +0800

    feat: 添加网关接口管理文档

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1203,7 +1203,8 @@ function genGatewaySidebar() {
     '/technique/16_网关服务/01_网关场景构架.md',
     '/technique/16_网关服务/02_网关配置.md',
     '/technique/16_网关服务/03_接口策略配置.md',
-    '/technique/16_网关服务/04_网关功能集成.md'
+    '/technique/16_网关服务/04_网关功能集成.md',
+    '/technique/16_网关服务/05_管理接口.md'
   ]
   return mapArr.map(i => {
     return i
